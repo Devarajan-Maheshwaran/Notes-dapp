0d91cfa04c2328fff3dadae1b1ffe73948594e50
Devarajan Maheshwaran <devarajanmaheswaran.cse2024@citchennai.net>
2025-07-10 13:17:08 +0530

    Update NoteContract.sol

--- contracts/NoteContract.sol
+++ contracts/NoteContract.sol
@@ -1,29 +1,59 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-contract NoteContract {
-    
-    mapping(address => string [] ) public notes;
+contract NotesContract {
+    uint public noteCount;
 
-    constructor() public {
-
+    struct Note {
+        uint id;
+        string title;
+        string content;
+        address author;
+        uint timestamp;
     }
 
-    event NewNote(address, string note);
-    event ModifyNote(address, uint index);
+    mapping(uint => Note) public notes;
 
-    function addNote( string memory note) public {
-    
-        notes[msg.sender].push(note);
-        emit NewNote(msg.sender, note);
+    event NoteCreated(uint indexed id, string title, address indexed author);
+    event NoteEdited(uint indexed id, string newTitle, string newContent);
+    event NoteDeleted(uint indexed id);
+
+    function createNote(string memory _title, string memory _content) public {
+        require(bytes(_title).length > 0, "Title cannot be empty");
+        require(bytes(_content).length > 0, "Content cannot be empty");
+
+        noteCount++;
+        notes[noteCount] = Note({
+            id: noteCount,
+            title: _title,
+            content: _content,
+            author: msg.sender,
+            timestamp: block.timestamp
+        });
+
+        emit NoteCreated(noteCount, _title, msg.sender);
     }
 
-    function getNotesLen(address own) public view returns (uint) {
-        return notes[own].length;
-    }
-    
-    function modifyNote(address own, uint index, string memory note) public {
-        notes[own][index] = note;
-        emit ModifyNote(own, index);
+    function getNote(uint _id) public view returns (Note memory) {
+        require(_id > 0 && _id <= noteCount, "Note does not exist");
+        return notes[_id];
     }
 
+    function editNote(uint _id, string memory _newTitle, string memory _newContent) public {
+        Note storage note = notes[_id];
+        require(note.author == msg.sender, "You are not the author");
+
+        note.title = _newTitle;
+        note.content = _newContent;
+
+        emit NoteEdited(_id, _newTitle, _newContent);
+    }
+
+    function deleteNote(uint _id) public {
+        Note storage note = notes[_id];
+        require(note.author == msg.sender, "You are not the author");
+
+        delete notes[_id];
+        emit NoteDeleted(_id);
+    }
 }
